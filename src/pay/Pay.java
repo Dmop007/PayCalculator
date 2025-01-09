955c490d7bf7f023c203e2a550956702d5dece48
Dmop007 <dmop007@gmail.com>
2025-01-08 23:59:24 -0500

    Initial commit

---
+++ src/pay/Pay.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pay;
+
+/**
+ *
+ * @author David
+ */
+public class Pay {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        javax.swing.JFrame frame = new PayFrame();
+        frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    
+}
